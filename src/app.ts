b5e44e1850f6ab66a1edec0ea83112d7a53121eb
GagaMen <12294922+GagaMen@users.noreply.github.com>
2021-07-05 20:43:16 +0200

    feat: add package dotenv to handle environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import e, { Request, Response } from 'express';
 
+// load environment variables
+dotenv.config();
+
 const app = e();
-const port = 8080;
+const port = process.env['SERVER_PORT'] ?? 8080;
 
 app.get('/', (_: Request, res: Response) => {
     res.send('Hello world!');
