d730286201ffd8bd2f7ae90efae09e4efcfa7381
GagaMen <12294922+GagaMen@users.noreply.github.com>
2021-07-08 21:34:52 +0200

    feat: add mongodb support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,41 @@
 import dotenv from 'dotenv';
-import e, { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import { Db, MongoClient } from 'mongodb';
 
 // load environment variables
 dotenv.config();
 
-const app = e();
+// init database
+const databaseUrl = process.env['MONGODB_CONNECTION'] ?? null;
+if (databaseUrl === null) {
+    throw Error('No database connection defined. Please check .env file.');
+}
+
+const client = new MongoClient(databaseUrl);
+
+void client
+    .connect()
+    .then(() => {
+        const db: Db = client.db(process.env['MONGODB_DATABASE']);
+        void db.command({ ping: 1 });
+        console.log('Connected successfully to database server');
+
+        const testCollection = db.collection('testCollection');
+        void testCollection
+            .find({ a: 1 })
+            .count()
+            .then((count) => {
+                if (count === 0) {
+                    void testCollection.insertOne({ a: 1, b: 2, c: 3, d: 4, e: 5 });
+                }
+            });
+    })
+    .catch((err: string) => {
+        console.error(err);
+        void client.close();
+    });
+
+const app = express();
 const port = process.env['SERVER_PORT'] ?? 8080;
 
 app.get('/', (_: Request, res: Response) => {
