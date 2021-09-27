ac87ed11d56f7afe7d65e0b8f21c4156d9adbdb5
GagaMen <12294922+GagaMen@users.noreply.github.com>
2021-09-27 21:44:45 +0200

    refactor(route): remove unnecessary hello route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { container } from 'tsyringe';
 import { AccountTypeRoutes } from './account-type/account-type.routes';
-import hello from './routes/hello';
 import { UserRoutes } from './user/user.routes';
 
 export default (): Router => {
     const app = Router();
-    hello(app);
 
     const userRoutes = container.resolve(UserRoutes);
     userRoutes.registerRoutes(app);
